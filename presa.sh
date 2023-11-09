61f940d26d552894e32b6f9aa733d8e3f9ca1faf
ahbucknell <128325035+ahbucknell@users.noreply.github.com>
2023-11-09 13:55:00 +0000

    Err handling arg input + extensions

--- presa.sh
+++ presa.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
-while getopts f flag
-do
-  case "${flag}" in
-    f) file=${OPTARG};;
+# Saved file flag argument and check no others are used.
+while getopts ":f:o:" flag; do
+  case $flag in
+    f)
+      file_name="$OPTARG";;
+    o)
+      output_dir="$OPTARG";;
+    *)
+      echo "Usage: $0 [-fo] [-f input] [-o output]"
+      exit 1;;
   esac
 done
 
-echo "File: $file"
 
+if ! [[ $# -ne 2 ]]
+then
+  echo 1>&2 "Minimum argiuments (-fo) not passed."
+  exit 1
+fi
 
+# Check for correct amino acid FASTA file types
+if ! [[ "$file_name" =~ ^.*\.(fasta|fas|fa|faa|mpfa)$ ]]  
+then
+  echo "Incorrect file type: use one of the following: .fasta, .fas, .fa, .faa, .mpfa"
+  exit 1
+fi
 
+echo "Input file: $file_name"
+echo "Output dir: $output_dir"
